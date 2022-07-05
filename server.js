ae8255d500ca73d9bba1d3fdf42cf7b54f11e541
ColemanMatthew-FS <MRColeman1@student.fullsail.edu>
2022-07-05 17:39:35 -0400

    Initial Project Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const app = express()
+
+ app.get("/", (req, res, next) =>{
+     res.json({
+         message: "Did you GET IT?"
+     });
+})
+
+//middleware modules fpr error handling
+//goes here, creates a new error, then gets told to go to the second midddleware
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND");
+    error.status = 404;
+    next(error);
+})
+
+//this middleware prints out the error
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message, 
+            status: error.status,
+            mathod: req.method
+        }
+    })
+})
+
+app.listen(process.env.port, () => console.log(`Starting server on port ${process.env.port}`))
